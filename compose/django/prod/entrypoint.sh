405218fd145c06ca76e4e1e43c85f6c44a1ee288
Radhika Nangia <radhikanangia23@gmail.com>
2023-03-10 17:06:53 +1100

    Create entrypoint.sh

---
+++ compose/django/prod/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+
+postgres_ready(){
+python << END
+import sys
+import psycopg2
+try:
+    psycopg2.connect(dbname="${POSTGRES_USER}", user="${POSTGRES_USER}", password="${POSTGRES_PASSWORD}", host="${POSTGRES_HOST}", port="${POSTGRES_PORT}",)
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing..."
+exec "$@"
